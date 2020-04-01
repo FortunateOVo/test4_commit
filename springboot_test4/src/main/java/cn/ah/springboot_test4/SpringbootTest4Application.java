2ca9c612ead11ddb549ad65bd4dd7e4a3969dcbb
ah <1837160850@qq.com>
2020-04-01 12:02:40 +0800

    test4 commit

---
+++ springboot_test4/src/main/java/cn/ah/springboot_test4/SpringbootTest4Application.java
@@ -0,0 +1,12 @@
+package cn.ah.springboot_test4;
+
+
+
+@SpringBootApplication
+public class SpringbootTest4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTest4Application.class, args);
+    }
+
+}
---
+++ springboot_test4/src/test/java/cn/ah/springboot_test4/SpringbootTest4ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ah.springboot_test4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTest4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
